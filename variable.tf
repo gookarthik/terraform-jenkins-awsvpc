d657813bcf3087630302dc7e6550b3888caf4c5a
gookarthik <gookarthik@gmail.com>
2022-12-09 13:48:24 +0530

    add

---
+++ variable.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-west-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "terraform-jenkins-awsvpc"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "security_jenkins_grp"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-Terraform_ec2-instance"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-094125af156557ca2"
+}
+
+
+variable "vpc-cidr" {
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR BLOCK"
+  type        = string
+}
+variable "Public_Subnet_1" {
+  default     = "10.0.0.0/24"
+  description = "Public_Subnet_1"
+  type        = string
+}
+variable "Private_Subnet_1" {
+  default     = "10.0.2.0/24"
+  description = "Private_Subnet_1"
+  type        = string
+}
